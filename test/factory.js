255be5eb373c14a1932a071dcfae723eb5452e60
Jaka Hudoklin <jakahudoklin@gmail.com>
2016-11-28 03:17:24 +0100

    fix naming from Error to GeneralError

--- test/factory.js
+++ test/factory.js
@@ -3,23 +3,23 @@
 const expect = require('chai').expect;
 
 const ErrorFactory = require('../lib/factory');
-const Error = require('../lib/error');
+const GeneralError = require('../lib/error');
 
 describe('ErrorFactory', () => {
     it('should create a new ErrorFactory', () => new ErrorFactory());
 
     it('should create error using ErrorFactory', () => {
         const errorFactory = new ErrorFactory();
-        const error = new errorFactory.Error({code: 'some_error_code'});
+        const error = new errorFactory.GeneralError({code: 'some_error_code'});
 
-        expect(error).to.be.instanceof(Error);
+        expect(error).to.be.instanceof(GeneralError);
     });
 
     it('should create error with context', () => {
         const errorFactory = new ErrorFactory({
             namespace: 'abcd'
         });
-        const error = new errorFactory.Error({code: 'some_error_code'});
+        const error = new errorFactory.GeneralError({code: 'some_error_code'});
 
         expect(error.extra).to.be.deep.equal({namespace: 'abcd'});
     });
